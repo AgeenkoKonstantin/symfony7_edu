fd172f277556d86db3eb99d1d33a59153c1b59e4
ageenko.k <kostik2000ag@gmail.com>
2024-10-11 00:36:19 +0300

    добавил бутстрап, м ко м связь блока и тегов, дата трансформер для тегов в форме блога

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -3,8 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\BlogRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\PersistentCollection;
 
 #[ORM\Entity(repositoryClass: BlogRepository::class)]
 class Blog
@@ -27,9 +29,33 @@ class Blog
     #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id')]
     private Category|null $category = null;
 
+    #[ORM\JoinTable(name: 'tags_to_blog')]
+    #[ORM\JoinColumn(name: 'blog_id', referencedColumnName: 'id')]
+    #[ORM\InverseJoinColumn(name: 'tag_id', referencedColumnName: 'id', unique: true)]
+    #[ORM\ManyToMany(targetEntity: 'App\Entity\Tag', cascade: ['persist'])]
+    private ArrayCollection|PersistentCollection $tags;
+
+    public function addTag(Tag $tag): void
+    {
+        $this->tags[] = $tag;
+    }
+
+
     public function getDescription(): ?string
     {
         return $this->description;
+    }
+
+    public function getTags(): ArrayCollection|PersistentCollection
+    {
+        return $this->tags;
+    }
+
+    public function setTags(ArrayCollection $tags): static
+    {
+        $this->tags = $tags;
+
+        return $this;
     }
 
     public function setDescription(?string $description): static
